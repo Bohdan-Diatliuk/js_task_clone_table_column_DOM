25e011024799ca94e0d79441596b5e39715f0736
Bohdan Diatliuk <dyatlyuk.bohdan@gmail.com>
2025-10-19 21:50:50 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const headRows = table.tHead.rows;
+const bodyRows = table.tBodies[0].rows;
+const footRows = table.tFoot.rows;
+
+function cloneColumn(fromIndex, toLastIndex) {
+  for (const row of headRows) {
+    const newCell = row.cells[fromIndex].cloneNode(true);
+
+    row.insertBefore(newCell, row.cells[toLastIndex]);
+  }
+
+  for (const row of bodyRows) {
+    const newCell = row.cells[fromIndex].cloneNode(true);
+
+    row.insertBefore(newCell, row.cells[toLastIndex]);
+  }
+
+  for (const row of footRows) {
+    const newCell = row.cells[fromIndex].cloneNode(true);
+
+    row.insertBefore(newCell, row.cells[toLastIndex]);
+  }
+}
+
+cloneColumn(1, 4);
