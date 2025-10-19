82ab2b463f569e1ce84b1798ae2d1f47e9a76b80
Bohdan Diatliuk <dyatlyuk.bohdan@gmail.com>
2025-10-19 22:07:45 +0300

    fix 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,18 +8,30 @@ const footRows = table.tFoot.rows;
 
 function cloneColumn(fromIndex) {
   for (const row of headRows) {
+    if (!row.cells.length) {
+      continue;
+    }
+
     const newCell = row.cells[fromIndex].cloneNode(true);
 
     row.insertBefore(newCell, row.cells[row.cells.length - 1]);
   }
 
   for (const row of bodyRows) {
+    if (!row.cells.length) {
+      continue;
+    }
+
     const newCell = row.cells[fromIndex].cloneNode(true);
 
     row.insertBefore(newCell, row.cells[row.cells.length - 1]);
   }
 
   for (const row of footRows) {
+    if (!row.cells.length) {
+      continue;
+    }
+
     const newCell = row.cells[fromIndex].cloneNode(true);
 
     row.insertBefore(newCell, row.cells[row.cells.length - 1]);
